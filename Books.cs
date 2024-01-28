2eb57cfb5ea2c8f3dff69236a8810bcf9003b86d
ahaanthakur887 <thakura@stpaulsschool.org.uk>
2024-01-28 23:47:45 +0000

    Add project files.

---
+++ Books.cs
@@ -0,0 +1,27 @@
+﻿namespace LibraryOOP
+{
+    public  class Books
+    {
+        // Fields
+        private bool Availability = true;
+
+        // Properties
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int PublicationDate { get; set; }
+
+        // Constructor
+        public Books(string title, string author, int publicationDate)
+        {
+            Title = title;
+            Author = author;
+            PublicationDate = publicationDate;
+        }
+
+        // Methods
+        public string BookInfo()
+        {
+            return $"Title is : {Title}, Author is {Author}, Publication date is {PublicationDate}.";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace LibraryOOP
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Books book = new Books("Harry Potter and the Philosopher's Stone", "J.K. Rowling", 1997);
+            Console.WriteLine(book.BookInfo());
+        }
+    }
+}
